4f4d795c58f260112e4f6520385f3a9b7ddd776d
shasherazi <hassanrandomz@gmail.com>
2023-01-24 17:42:54 +0500

    Add tests

---
+++ tests/stringLength.test.js
@@ -0,0 +1,11 @@
+const stringLength = require('../src/stringLength.js');
+
+test('Length of a string', () => {
+  expect(stringLength('abcde')).toBe(5);
+});
+
+test('Length of a string with empty string', () => {
+  expect(() => {
+    stringLength('');
+  }).toThrow();
+});
